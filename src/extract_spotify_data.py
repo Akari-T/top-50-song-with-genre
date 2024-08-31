4c2c8da9283a7f27a2cb7e56a1e23d44909074e5
Akari-T <46663133+Akari-T@users.noreply.github.com>
2024-08-30 21:05:11 -0700

    added artist genre

--- src/extract_spotify_data.py
+++ src/extract_spotify_data.py
@@ -1,4 +1,5 @@
 import spotipy
+import numpy as np
 from spotipy.oauth2 import SpotifyClientCredentials
 from datetime import datetime
 from dotenv import load_dotenv
@@ -7,12 +8,15 @@ import os
 
 load_dotenv()
 
+# Confirmed run!
+# The modification would be to get the genre infor from the artist.
 def extract_data(playlist_id, country):
 
     # The csv file storing the top 50
-    csv_file_path = './data/spotify-streaming-top-50-' + country + '.csv'
+    csv_file_path = './data/with-genre/spotify-streaming-top-50-' + country + '-with-genre.csv'
     today = datetime.today().strftime('%Y-%m-%d')
 
+    ## Boolean to check if today's top 50 has been recorded or not
     date_processed = False
     if os.path.exists(csv_file_path):
         with open(csv_file_path, 'r', encoding="utf-8") as file:
@@ -25,8 +29,6 @@ def extract_data(playlist_id, country):
                     break
 
     # Credentials to access Spotify API
-    #client_id = os.environ["CLIENT_ID"]
-    #client_secret = os.environ["CLIENT_SECRET"]
     client_id = os.getenv('SPOTIFY_CLIENT_ID')
     client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
@@ -46,9 +48,9 @@ def extract_data(playlist_id, country):
         # Obtaining tracks data and sorting it by field popularity (indicator of streaming count)
         ## sorted_tracks = sorted(uris, key=lambda uri: sp.track(uri)['popularity'], reverse=True)
 
-        # Creating list of lists and including the header of the final CSV file 
+        # Creating list of lists and including the header of the final CSV file
         spotify_data = []
-        head = ['date', 'position', 'song', 'artist', 'popularity', 'duration_ms', 'album_type', 'total_tracks', 'release_date', 'is_explicit', 'album_cover_url']
+        head = ['date', 'position', 'song', 'artist', 'popularity', 'duration_ms', 'album_type', 'total_tracks', 'release_date', 'is_explicit', 'album_cover_url', 'genre_list']
         ## spotify_data.append(head)
 
         # Getting the data obtained from Spotify API into the list of lists
@@ -56,7 +58,12 @@ def extract_data(playlist_id, country):
             track = sp.track(uri)
 
             song = track['name']
-            artist_aux = [artist['name'] for artist in track['album']['artists']]
+            # artist_aux = [artist['name'] for artist in track['album']['artists']]
+            artist_aux = [artist['name'] for artist in track['artists']]
+            artist_ids = [artist['id'] for artist in track['artists']]  # mine
+            genres = [sp.artist(artist_id)['genres'] for artist_id in artist_ids]
+            genres = [genre for genre_lst in genres for genre in genre_lst]
+            genres = np.unique(genres)
             if len(artist_aux) > 1:
                 artist = artist_aux[0] + ' & ' + ' & '.join(artist_aux[1:])
             else:
@@ -68,8 +75,14 @@ def extract_data(playlist_id, country):
             release_date = track['album']['release_date']
             is_explicit = track['explicit']
             album_cover_url = track['album']['images'][0]['url']
+            if len(genres) > 1:
+                genre_list = genres[0] + ' & ' + ' & '.join(genres[1:])
+            elif len(genres) == 1:
+                genre_list = genres[0]
+            else: # For debug purpose
+                genre_list = []
 
-            spotify_data.append([today, rank+1, song, artist, popularity, duration_ms, album_type, total_tracks, release_date, is_explicit, album_cover_url])
+            spotify_data.append([today, rank+1, song, artist, popularity, duration_ms, album_type, total_tracks, release_date, is_explicit, album_cover_url, genre_list])
 
         ## Pass data into a csv file
         if not os.path.exists(csv_file_path):
